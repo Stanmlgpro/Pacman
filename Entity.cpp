b34b9bc1b3c11a3231901e49878dd50c83c023aa
stan1 <stan1van2campenhout3@gmail.com>
2025-10-13 19:17:56 +0200

    first class

---
+++ Entity.cpp
@@ -0,0 +1,5 @@
+//
+// Created by stan1 on 13/10/2025.
+//
+
+#include "Entity.h"
---
+++ Entity.h
@@ -0,0 +1,16 @@
+//
+// Created by stan1 on 13/10/2025.
+//
+
+#ifndef ENTITY_H
+#define ENTITY_H
+
+
+
+class Entity {
+
+};
+
+
+
+#endif //ENTITY_H
