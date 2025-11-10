30ff69355f242cf3df5aa5d78a398601fba0289b
stan1 <stan1van2campenhout3@gmail.com>
2025-11-10 17:43:56 +0100

    added random class

---
+++ singleton/Random.h
@@ -0,0 +1,26 @@
+//
+// Created by stan1 on 10/11/2025.
+//
+
+#ifndef RANDOM_H
+#define RANDOM_H
+
+
+
+class Random {
+private:
+    std::mt19937 gen;
+
+    Random();
+public:
+    Random(const Random&) = delete;
+    Random& operator=(const Random&) = delete;
+
+    int get(int min, int max);
+
+    static Random& getInstance();
+};
+
+
+
+#endif //RANDOM_H
