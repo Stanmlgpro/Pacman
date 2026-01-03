6b6fa2fc2b10cf02bde21df4bf92dc004dd19fcb
GitHub Action (clang-format) <action@github.com>
2026-01-03 21:20:50 +0000

    style: auto-format C++ code with clang-format-18

--- World.h
+++ World.h
@@ -33,37 +33,37 @@ enum MOVE { UP, DOWN, LEFT, RIGHT };
 class World : public std::enable_shared_from_this<World> {
 public:
     /**
- * @brief Creates a StateManager object.
- * @param filename File from which we read the map
- * @param entity_factory Reference to the entity_factory so we can gcreate the necessary entites
- * @param world_sounds Reference to the world_sounds so we can create the necessary sounds
- * @param player to give to the Score class
- */
+     * @brief Creates a StateManager object.
+     * @param filename File from which we read the map
+     * @param entity_factory Reference to the entity_factory so we can gcreate the necessary entites
+     * @param world_sounds Reference to the world_sounds so we can create the necessary sounds
+     * @param player to give to the Score class
+     */
     explicit World(std::string filename, std::shared_ptr<factory::EntityFactory> entity_factory,
                    std::shared_ptr<sounds::WorldSound> world_sounds, std::string player);
 
     /**
- * @brief Called when a new map needs to be loaded
- *
- * reads from the file and creates the correct objects using the entity_factory
- * also called when a level is won, and we need to go to a new level
- * also upgrades the difficulty by shortening fearmode and increasing ghost speed
- */
+     * @brief Called when a new map needs to be loaded
+     *
+     * reads from the file and creates the correct objects using the entity_factory
+     * also called when a level is won, and we need to go to a new level
+     * also upgrades the difficulty by shortening fearmode and increasing ghost speed
+     */
     void loadMap_reset();
     /**
-* @brief Updates the game and its entities
-*
-* first calculates the new dt using the Stopwatch
-* then checks for a won or lost game and tries the buffered input
-* then test all collisions, removes all deleted entities and updates all the rest
-*/
+     * @brief Updates the game and its entities
+     *
+     * first calculates the new dt using the Stopwatch
+     * then checks for a won or lost game and tries the buffered input
+     * then test all collisions, removes all deleted entities and updates all the rest
+     */
     bool Update();
     /**
-* @brief tests if a specific entity is interaction with pacman
-*
-* calculates whether are not pacman is hitting an entity
-* then deletes, adds scoring, ... based on which entity hits pacman
-*/
+     * @brief tests if a specific entity is interaction with pacman
+     *
+     * calculates whether are not pacman is hitting an entity
+     * then deletes, adds scoring, ... based on which entity hits pacman
+     */
     /// @{
     std::shared_ptr<entities::Entity> CollidesWithPacman(std::shared_ptr<entities::Wall> wall);
     std::shared_ptr<entities::Entity> CollidesWithPacman(std::shared_ptr<entities::Orb> orb);
@@ -74,44 +74,44 @@ public:
     /// @}
 
     /**
-* @brief checks the inputted buffer
-*
-* snaps pacman to grid, checks if the direction would be possible if we were at that point
-* swaps the direction in for the buffer is we are close enough to the tile center
-*/
+     * @brief checks the inputted buffer
+     *
+     * snaps pacman to grid, checks if the direction would be possible if we were at that point
+     * swaps the direction in for the buffer is we are close enough to the tile center
+     */
     void TryBuffer();
     /**
-* @brief Helper function
-*
-* calculates the closest tile to the world coords
-*/
+     * @brief Helper function
+     *
+     * calculates the closest tile to the world coords
+     */
     static std::vector<int> NormalizedToGrid(float normX, float normY, std::vector<std::vector<bool>> wallGrid);
     /**
-* @brief Renders the game
-*
-* renders all the entites
-* renders pacman
-* renders the worldview
-*/
+     * @brief Renders the game
+     *
+     * renders all the entites
+     * renders pacman
+     * renders the worldview
+     */
     void Render();
     /**
-* @brief sets pacman's directions
-*
-* sets the buffered direction of pacman to whatever was inputted
-*/
+     * @brief sets pacman's directions
+     *
+     * sets the buffered direction of pacman to whatever was inputted
+     */
     void movePacman(MOVE movement);
 
     /**
- * @return returns the Entities
- */
+     * @return returns the Entities
+     */
     std::vector<std::shared_ptr<entities::Entity>> getEntities();
     /**
- * @return returns the amount of Lives
- */
+     * @return returns the amount of Lives
+     */
     int getLives() const;
     /**
- * @return returns the Score
- */
+     * @return returns the Score
+     */
     int getScore() const;
 
     ~World();
